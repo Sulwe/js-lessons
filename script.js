5f2aa2a1d2c07cb005ba4e84f02e9e07f245a3b3
Sulwe <cypriansulwe.cs@gmail.com>
2023-09-21 14:47:11 +0300

    create a button that greets when pressed

---
+++ script.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector("button");
+
+
+function greet() {
+    const name = prompt("What is your name?");
+    const greeting = document.querySelector("#greeting");
+    greeting.textContent = `Hello ${name}, nice to see you!`;
+}
+
+btn.addEventListener("click", greet);
